d733e180cbb5e3d03d1df3191a9eaa666ace54ce
Pradeepss95 <pradeepss95@gmail.com>
2025-06-04 20:37:22 +0100

    Cucumber Spring Template

---
+++ src/test/java/org/example/restapitest/runner/TestRunnerObjectFeature.java
@@ -0,0 +1,17 @@
+package org.example.restapitest.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        glue = "org/example/restapitest/stepdefinitions",
+        features = "src/test/resources/features")
+
+public class TestRunnerObjectFeature extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
